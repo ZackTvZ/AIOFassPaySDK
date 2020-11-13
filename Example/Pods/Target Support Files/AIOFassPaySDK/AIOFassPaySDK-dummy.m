c02c57d72e943a15ca2d19edd8a59d7f4b314869
Zack <zacktvz@hotmail.my>
2020-11-13 23:11:02 +0800

    first commit

---
+++ Example/Pods/Target Support Files/AIOFassPaySDK/AIOFassPaySDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AIOFassPaySDK : NSObject
+@end
+@implementation PodsDummy_AIOFassPaySDK
+@end
---
+++ Example/Pods/Target Support Files/Pods-AIOFassPaySDK_Example/Pods-AIOFassPaySDK_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AIOFassPaySDK_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AIOFassPaySDK_Example
+@end
